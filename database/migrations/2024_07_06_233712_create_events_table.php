1379cb9c28396291f4571bb2fd9c1dd3709c1e4c
Awaluddin Kasim <awaluddinkasim7@gmail.com>
2024-07-07 08:09:17 +0800

    update

---
+++ database/migrations/2024_07_06_233712_create_events_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama');
+            $table->string('lokasi');
+            $table->string('deskripsi');
+            $table->string('latitude');
+            $table->string('longitude');
+            $table->string('trailer');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('events');
+    }
+};
