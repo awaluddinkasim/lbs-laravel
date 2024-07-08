5ff65a20041b67713c843d966e1e43e3a6a00cb1
Awaluddin Kasim <awaluddinkasim7@gmail.com>
2024-07-09 07:58:57 +0800

    tambah attribute href pada buttons

--- resources/views/components/component/button-soft.blade.php
+++ resources/views/components/component/button-soft.blade.php
@@ -1,6 +1,7 @@
-@props(['label', 'type' => 'button', 'color' => 'primary', 'block' => false])
+@props(['label', 'type' => 'button', 'color' => 'primary', 'block' => false, 'href' => 'null'])
 
 <button type="{{ $type }}"
-    class="btn bg-{{ $color }}/25 text-{{ $color }} hover:bg-{{ $color }} hover:text-white {{ $block ? 'w-full' : '' }}">
+    class="btn bg-{{ $color }}/25 text-{{ $color }} hover:bg-{{ $color }} hover:text-white {{ $block ? 'w-full' : '' }}"
+    {!! $href ? 'onclick="document.location.href = \'' . $href . '\'"' : '' !!}>
     {{ $label }}
 </button>
--- resources/views/components/component/button.blade.php
+++ resources/views/components/component/button.blade.php
@@ -1,7 +1,8 @@
-@props(['label', 'type' => 'button', 'color' => 'primary', 'block' => false])
+@props(['label', 'type' => 'button', 'color' => 'primary', 'block' => false, 'href' => null])
 
 <div class="my-2">
-    <button type="{{ $type }}" class="text-white btn bg-{{ $color }} {{ $block ? 'w-full' : '' }}">
+    <button type="{{ $type }}" class="text-white btn bg-{{ $color }} {{ $block ? 'w-full' : '' }}"
+        {!! $href ? 'onclick="document.location.href = \'' . $href . '\'"' : '' !!}>
         {{ $label }}
     </button>
 </div>
