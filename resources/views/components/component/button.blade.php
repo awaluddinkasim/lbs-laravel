7a3132966295c6f06ca364b2278a603793983ef4
Awaluddin Kasim <awaluddinkasim7@gmail.com>
2024-07-09 00:20:38 +0800

    update button component

--- resources/views/components/component/button.blade.php
+++ resources/views/components/component/button.blade.php
@@ -1,5 +1,7 @@
 @props(['label', 'type' => 'button', 'color' => 'primary', 'block' => false])
 
-<button type="{{ $type }}" class="text-white btn bg-{{ $color }} {{ $block ? 'w-full' : '' }}">
-    {{ $label }}
-</button>
+<div class="my-2">
+    <button type="{{ $type }}" class="text-white btn bg-{{ $color }} {{ $block ? 'w-full' : '' }}">
+        {{ $label }}
+    </button>
+</div>
