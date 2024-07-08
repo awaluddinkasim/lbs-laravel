5446114dab2b4271d4ca6f2c27ccca59268687a3
Awaluddin Kasim <awaluddinkasim7@gmail.com>
2024-07-09 07:26:29 +0800

    base controller

---
+++ app/Http/Controllers/BaseController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+
+class BaseController extends Controller
+{
+    public function redirect(string $redirectTo, array $response): RedirectResponse
+    {
+        return redirect($redirectTo)->with(
+            $response['status'],
+            $response['message']
+        );
+    }
+
+    public function redirectWithInput(string $redirectTo, array $request, array $response): RedirectResponse
+    {
+        return redirect($redirectTo)->with(
+            $response['status'],
+            $response['message']
+        )->withInput($request);
+    }
+
+    public function jsonResponse(array $data, int $statusCode = 200): JsonResponse
+    {
+        return response()->json($data, $statusCode);
+    }
+}
