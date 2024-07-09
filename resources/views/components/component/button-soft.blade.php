f2bd5c9c49649204b41e49cf400acf949d571249
Awaluddin Kasim <awaluddinkasim7@gmail.com>
2024-07-09 08:52:14 +0800

    update

--- resources/views/components/component/button-soft.blade.php
+++ resources/views/components/component/button-soft.blade.php
@@ -1,4 +1,4 @@
-@props(['label', 'type' => 'button', 'color' => 'primary', 'block' => false, 'href' => 'null'])
+@props(['label', 'type' => 'button', 'color' => 'primary', 'block' => false, 'href' => null])
 
 <button type="{{ $type }}"
     class="btn bg-{{ $color }}/25 text-{{ $color }} hover:bg-{{ $color }} hover:text-white {{ $block ? 'w-full' : '' }}"
