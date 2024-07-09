995624f5fdc58a55d6c81a064770b2c0c7c63e16
Awaluddin Kasim <awaluddinkasim7@gmail.com>
2024-07-09 08:09:24 +0800

    remove margin

--- resources/views/components/component/button.blade.php
+++ resources/views/components/component/button.blade.php
@@ -1,8 +1,6 @@
 @props(['label', 'type' => 'button', 'color' => 'primary', 'block' => false, 'href' => null])
 
-<div class="my-2">
-    <button type="{{ $type }}" class="text-white btn bg-{{ $color }} {{ $block ? 'w-full' : '' }}"
-        {!! $href ? 'onclick="document.location.href = \'' . $href . '\'"' : '' !!}>
-        {{ $label }}
-    </button>
-</div>
+<button type="{{ $type }}" class="text-white btn bg-{{ $color }} {{ $block ? 'w-full' : '' }}"
+    {!! $href ? 'onclick="document.location.href = \'' . $href . '\'"' : '' !!}>
+    {{ $label }}
+</button>
