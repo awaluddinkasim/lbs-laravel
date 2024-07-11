6cb6693024560ff8715d4d7c0dfb858db149d2b9
Awaluddin Kasim <awaluddinkasim7@gmail.com>
2024-07-11 08:34:32 +0800

    auth

--- app/Http/Controllers/User/AuthController.php
+++ app/Http/Controllers/User/AuthController.php
@@ -2,10 +2,46 @@
 
 namespace App\Http\Controllers\User;
 
+use App\Http\Controllers\BaseController;
 use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
-class AuthController extends Controller
+class AuthController extends BaseController
 {
-    //
+    public function login(Request $request): JsonResponse
+    {
+        if (Auth::attempt($request->only('email', 'password'))) {
+            $user = User::find(Auth::user()->id);
+
+            if (!$user->hasVerifiedEmail()) {
+                Auth::logout();
+                return $this->jsonResponse([
+                    'message' => 'Email belum diverifikasi!'
+                ], 401);
+            }
+
+            return $this->jsonResponse([
+                'status' => 'success',
+                'user' => $user,
+                'token' => $user->createToken('authToken')->plainTextToken
+            ]);
+        }
+
+        return $this->jsonResponse([
+            'message' => 'Email atau Password salah!'
+        ], 401);
+    }
+
+    public function logout(Request $request): JsonResponse
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return $this->jsonResponse([
+            'status' => 'success',
+            'message' => 'Logout berhasil'
+        ]);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\User\AuthController;
 use App\Http\Controllers\User\EventController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -8,5 +9,11 @@ use Illuminate\Support\Facades\Route;
 //     return $request->user();
 // })->middleware('auth:sanctum');
 
-Route::get('/events', [EventController::class, 'get']);
-Route::get('/events/search', [EventController::class, 'search']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::group(['middleware' => 'auth:sanctum'], function () {
+    Route::get('/events', [EventController::class, 'get']);
+    Route::get('/events/search', [EventController::class, 'search']);
+
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
