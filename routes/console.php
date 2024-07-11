58e9e8ceae5da35456c1facc64781f343c858098
ipi <stnrfzhlfnksm21@gmail.com>
2024-07-11 15:17:00 +0800

    update status event setiap hari

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,25 @@
 <?php
 
+use App\Models\Event;
+use Carbon\Carbon;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Schedule::call(function () {
+    $events = Event::where('status', 'aktif')->get();
+
+    foreach ($events as $event) {
+        $tanggalSelesai = Carbon::parse($event->tanggal_selesai);
+
+        if ($tanggalSelesai->lt(Carbon::now())) {
+            $event->update([
+                'status' => 'selesai',
+            ]);
+        }
+    }
+})->daily();
