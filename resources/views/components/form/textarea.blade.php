97bd1dc230606a63cee154c827f06c5fdb7ba05c
ipi <stnrfzhlfnksm21@gmail.com>
2024-07-12 15:19:40 +0800

    tambahkan attribute rows

--- resources/views/components/form/textarea.blade.php
+++ resources/views/components/form/textarea.blade.php
@@ -1,7 +1,8 @@
-@props(['label', 'id', 'name', 'required' => false])
+@props(['label', 'id', 'name', 'rows' => 2, 'required' => false])
 
 <div class="mb-3">
     <label for="{{ $id }}"
         class="text-gray-800 text-sm font-medium inline-block mb-2">{{ $label }}</label>
-    <textarea id="{{ $id }}" name="{{ $name }}" class="form-input" {{ $required ? 'required' : '' }}>{{ $slot }}</textarea>
+    <textarea id="{{ $id }}" name="{{ $name }}" class="form-input" {{ $required ? 'required' : '' }}
+        rows="{{ $rows }}">{{ $slot }}</textarea>
 </div>
