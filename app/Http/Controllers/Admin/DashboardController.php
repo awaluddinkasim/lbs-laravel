adb2344103001f2f8bd74088b083121e174ababe
ipi <stnrfzhlfnksm21@gmail.com>
2024-07-12 19:08:15 +0800

    dashboard

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Event;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\View\View;
 
@@ -10,6 +12,11 @@ class DashboardController extends Controller
 {
     public function index(): View
     {
-        return view('pages.dashboard');
+        return view('pages.dashboard', [
+            'totalEvents' => Event::all()->count(),
+            'eventsAktif' => Event::where('status', 'aktif')->get()->count(),
+            'eventsSelesai' => Event::where('status', 'selesai')->get()->count(),
+            'users' => User::all()->count(),
+        ]);
     }
 }
