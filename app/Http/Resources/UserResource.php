11dd43dcbf879fd459683eb668514666aa3a857c
ipi <stnrfzhlfnksm21@gmail.com>
2024-07-12 16:44:42 +0800

    carbon pada tanggal

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -18,7 +19,7 @@ class UserResource extends JsonResource
             'email' => $this->email,
             'nama' => $this->nama,
             'no_hp' => $this->no_hp,
-            'tgl_lahir' => $this->tgl_lahir,
+            'tgl_lahir' => Carbon::parse($this->tgl_lahir)->isoFormat('DD MMMM YYYY'),
             'jenis_kelamin' => $this->jenis_kelamin,
             'alamat' => $this->alamat,
         ];
